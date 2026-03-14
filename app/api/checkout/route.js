28327f32f171430537e471b2fcd1d75c5f72b62d
Karl <karlsennek@gmail.com>
2026-03-14 21:28:51 +0100

    Fix dynamic stripe import

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,7 +1,9 @@
-import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function POST() {
+  const Stripe = (await import("stripe")).default;
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
   const session = await stripe.checkout.sessions.create({
